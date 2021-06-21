03be2e37c33df961e5908caf490f0c910e11f8d9
Abdullah272056 <abdullah272056@gmail.com>
2021-06-21 14:17:40 +0600

    create runTimePermission method for getting permission

--- app/src/main/java/com/example/pdfreader/MainActivity.java
+++ app/src/main/java/com/example/pdfreader/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.pdfreader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    PdfAdapter pdfAdapter;
+    RecyclerView recyclerView;
+    List<File> pdfList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void runTimePermission(){
+        Dexter.withContext(this)
+                .withPermission(
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                ).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+           // displayPdf();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Toast.makeText(MainActivity.this, "permission required", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+
     }
 }
